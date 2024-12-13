5f6b37bb27e79165a91b30586ac951b22567dac2
aceria / albertoceria <alberto.ceria@outlook.com>
2024-12-13 14:13:40 +0100

    Update utilities.py
    updated utilities, it takes a list of links or of hyperlinks and labels as input

--- utilities.py
+++ utilities.py
@@ -1,63 +1,79 @@
 import pandas as pd
 from collections import Counter
-import networkx as nx
-from collections.abc import Iterable
-
-
+from itertools import chain
+import numpy as np
 def get_bipartite_hyper(hyper_df):
-    return hyper_df.explode(['BvD','ISO'])
+    return hyper_df.explode(['nodes','labels'])
 
-def add_national_share(df):
+def add_same_label_share(df):
     """
-    Adds national share information to the given DataFrame.
+    Adds same_label share information to the given DataFrame.
 
     Parameters:
-    df (pandas.DataFrame): The DataFrame to which the national share information will be added.
+    df (pandas.DataFrame): The DataFrame to which the same_label share information will be added.
 
     Returns:
-    pandas.DataFrame: The DataFrame with the added national share information.
+    pandas.DataFrame: The DataFrame with the added same_label share information.
     """
     
-    df['national_share'] = df['ISO'].apply(lambda x:{k:v/len(x) for k,v in Counter(x).items()})
-    df['links'] = df['ISO'].apply(lambda x:tuple(sorted(set(x))))
-    df['max_country'] = df['national_share'].apply(lambda x:max(x, key=x.get))
-    df['max_country_share'] = df['national_share'].apply(lambda x:max(x.values()))
-    df['national_count'] = df['ISO'].apply(lambda x:Counter(x))
+    df['same_label_share'] = df['labels'].apply(lambda x:{k:v/len(x) for k,v in Counter(x).items()})
+    df['links'] = df['labels'].apply(lambda x:tuple(sorted(set(x))))
+    df['max_labels'] = df['same_label_share'].apply(lambda x:max(x, key=x.get))
+    df['max_labels_share'] = df['same_label_share'].apply(lambda x:max(x.values()))
+    df['same_label_count'] = df['labels'].apply(lambda x:Counter(x))
 
     return df
 
 
-def get_df_hyper(size_lim=50, revenue_limit='overall', type_data = 'board',unweighted = False):
+def from_bipartite_list_to_hyper_df(bipartite_list, label_list = None, max_order = False, unweighted = False):
+    df_hyper = pd.DataFrame(bipartite_list)
+    df_hyper.columns = ['nodes','hyperlink_id']
+    df_hyper = df_hyper.groupby('hyperlink_id')['nodes'].apply(lambda x:tuple(sorted(set(x)))).reset_index()
+    df_hyper['order'] = df_hyper['nodes'].apply(lambda x:len(x))
+    if label_list:
+        nodes = sorted(set([x[0] for x in bipartite_list]))
+        labels = {k:v for k,v in zip(nodes,label_list)}
+        df_hyper['labels'] = df_hyper['nodes'].apply(lambda x:tuple([labels[y] for y in x]))
+    if max_order:
+        df_hyper = df_hyper[df_hyper['order']<=max_order]
+    if unweighted:
+        df_hyper = df_hyper.drop_duplicates('nodes')
+    return df_hyper
+
+def from_hyperlink_list_to_hyper_df(hyperlink_list, label_list = None, max_order = False, unweighted = False):
+    df_hyper = pd.Series(hyperlink_list).reset_index()
+    df_hyper.columns = ['hyperlink_id','nodes']
+    df_hyper['nodes'] = df_hyper['nodes'].apply(lambda x:tuple(sorted(set(x))))
+    df_hyper['order'] = df_hyper['nodes'].apply(lambda x:len(x))
+    if label_list:
+        nodes = sorted(set(chain.from_iterable(hyperlink_list)))
+        labels = {k:v for k,v in zip(nodes,label_list)}
+        df_hyper['labels'] = df_hyper['nodes'].apply(lambda x:tuple([labels[y] for y in x]))
+    if max_order:
+        df_hyper = df_hyper[df_hyper['order']<=max_order]
+    if unweighted:
+        df_hyper = df_hyper.drop_duplicates('nodes')
+    return df_hyper
+
+
+def get_df_hyper(link_list,label_list=False, size_lim=False,unweighted = False):
     """
     Load and preprocess the DataFrame from a CSV file.
 
     Args:
-        size_lim (int, optional): The size limit for the DataFrame. Defaults to 50.
-        revenue_limit (str, optional): The revenue limit for the DataFrame. Defaults to 'overall'.
-        type_data (str, optional): The type of data to load. Defaults to 'board'.
+        link_list (list): Either a list of links of a bipartite network (node,hyperlink_id) tuples or a list of hyperlinks (v1,...,vn).
+        label_list (list, optional): A list of labels for the nodes, ordered by the node index.
+        size_lim (int, optional): The maximum order of hyperlinks to consider. Defaults to False.
+        unweighted (bool, optional): Whether to consider the hypergraph as unweighted. Defaults to False.
     Returns:
-        pandas.DataFrame: The preprocessed DataFrame.
+        pandas.DataFrame: The preprocessed DataFrame with columns 'nodes', 'hyperlink_id', 'order', and 'labels'(if available).
     """
     # Read the CSV file into a DataFrame
-   
-       
-    if type_data == 'board':
-         if revenue_limit == 'overall':df = pd.read_csv('/data/ceriaac/CORPNET_project/Data/Preprocessed/all_position_static_keep_inconsistent_ISO/Numbered/higher_order_com_nodes/all_positions_static_clean_'+str(size_lim)+'_'+revenue_limit+'_higher_order.gz')
-         else: df = pd.read_csv('/data/ceriaac/CORPNET_project/Data/Preprocessed/all_position_static_keep_inconsistent_ISO/Numbered/higher_order_com_nodes/all_positions_static_clean_overall_'+revenue_limit+'_higher_order.gz')
-         df = df[df['order']<=size_lim]
-    elif type_data == 'collaboration_diff_iso':
-        df = pd.read_csv('/data/ceriaac/CORPNET_project/Data/Preprocessed/collaboration/Numbered/higher_order/collaboration_iso_diff_static_clean_overall_overall_higher_order.gz')
-
-    elif type_data == 'collaboration_max_iso':
-        df = pd.read_csv('/data/ceriaac/CORPNET_project/Data/Preprocessed/collaboration/Numbered/higher_order/collaboration_iso_max_static_clean_overall_overall_higher_order.gz')
-
-    # Convert the 'ISO' and 'BvD' columns from string representation to tuples
-    df['ISO'] = df['ISO'].apply(lambda x: tuple(eval(x)))
-    df['BvD'] = df['BvD'].apply(lambda x: tuple(eval(x)))
-    df['ISO'] = df['ISO'].fillna('NA')
-    df.columns = ['fullName', 'BvD', 'ISO','order'] + list(df.columns[4:])
-    if unweighted:
-        df.drop_duplicates('BvD',inplace = True)
+    try:
+        df = from_bipartite_list_to_hyper_df(link_list, label_list, max_order = size_lim, unweighted = unweighted)
+    except:
+        df = from_hyperlink_list_to_hyper_df(link_list, label_list, max_order = size_lim, unweighted = unweighted)
+    df = df[df['order'] > 1]
     return df
 
 def from_bipartite_to_hyper_df(df):
@@ -71,19 +87,19 @@ def from_bipartite_to_hyper_df(df):
         pandas.DataFrame: The resulting DataFrame representing a hypergraph.
     """
     # Create a new DataFrame with index as unique full names
-    df_hyper = pd.DataFrame(index=sorted(df.fullName.unique()))
+    df_hyper = pd.DataFrame(index=sorted(df.hyperlink_id.unique()))
 
-    # Group the DataFrame by full name and get the unique sorted set of BvD values for each group
-    hyper_companies = df.groupby('fullName')['BvD'].apply(lambda x: tuple(sorted(set(x))))
+    # Group the DataFrame by full name and get the unique sorted set of nodes values for each group
+    hyper_companies = df.groupby('hyperlink_id')['nodes'].apply(lambda x: tuple(sorted(set(x))))
 
-    # Group the DataFrame by full name and get the unique sorted set of ISO values for each group
-    hyper_countries = df.groupby('fullName')['ISO'].apply(lambda x: tuple(sorted((x))))
+    # Group the DataFrame by full name and get the unique sorted set of labels values for each group
+    hyper_countries = df.groupby('hyperlink_id')['labels'].apply(lambda x: tuple(sorted((x))))
 
     # Assign the hyper_companies and hyper_countries values to the corresponding columns in df_hyper
-    df_hyper['BvD'] = hyper_companies
-    df_hyper['ISO'] = hyper_countries 
+    df_hyper['nodes'] = hyper_companies
+    df_hyper['labels'] = hyper_countries
 
-    # Calculate the order of each hypergraph by counting the number of BvD values in each group
+    # Calculate the order of each hypergraph by counting the number of nodes values in each group
     df_hyper['order'] = hyper_companies.apply(lambda x: len(x))
     
     
@@ -91,10 +107,5 @@ def from_bipartite_to_hyper_df(df):
     return df_hyper
 
 
-def remove_hubs(df,max_degree):
-    df_bi =  get_bipartite_hyper(df)
-    board_size = df_bi.groupby('BvD').size()
-    busy_board = set(board_size[board_size>max_degree].index)
-    df_bi = df_bi[(df['BvD'].isin(busy_board) == False)]
-    return from_bipartite_to_hyper_df(df_bi)
-
+def get_order_relevance(sr):
+    return (np.trapz(y = sr.values,x = sr.index)-1)/(max(sr.index)-min(sr.index)-1)
